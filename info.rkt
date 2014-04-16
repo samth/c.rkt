2c77efc34f284f66931587bed295c1c7fa7785ca
Sam Tobin-Hochstadt <samth@racket-lang.org>
2014-04-16 12:41:06 -0400

    Fix dependencies.

--- info.rkt
+++ info.rkt
@@ -1,4 +1,11 @@
 #lang setup/infotab
 (define collection 'multi)
-(define deps (list "abnf"))
-
+(define deps (list "abnf" "base" "parser-tools-lib"))
+(define build-deps '("at-exp-lib"
+                     "parser-tools-doc"
+                     "planet-doc"
+                     "racket-doc"
+                     "rackunit-lib"
+                     "scribble-doc"
+                     "scribble-lib"
+                     "srfi-lite-lib"))
